3aadac03cd0d747215d519608cb145d7eccfcfc9
HawkingHuang <math0915@gmail.com>
2024-02-12 20:29:28 +0800

    Remove vuetify.min.css for test

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import "../node_modules/vuetify/dist/vuetify.min.css";
+// import "../node_modules/vuetify/dist/vuetify.min.css";
 import { loadFonts } from "./plugins/webfontloader";
 
 loadFonts();
