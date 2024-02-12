94d1c5daa24185fb9955f327a181df4f989bc68a
HawkingHuang <math0915@gmail.com>
2024-02-12 20:54:44 +0800

    Import 'vuetify/dist/vuetify.min.css' to main.js for test

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 // import "../node_modules/vuetify/dist/vuetify.min.css";
+import "vuetify/dist/vuetify.min.css";
 import { loadFonts } from "./plugins/webfontloader";
 
 loadFonts();
